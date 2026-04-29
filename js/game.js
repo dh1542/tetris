2ee0bd8af6c3020564ec5fd28850d543589dfb84
Dominik Hoffmann <dominik151099@outlook.de>
2026-04-29 21:40:15 +0200

    feat: early game mechanic

--- js/game.js
+++ js/game.js
@@ -1,19 +1,19 @@
-// import { isBoardAdvanceable, advanceBoard } from "./board.js";
-// import { drawBoard } from "./render.js";
+import { isBoardAdvanceable, advanceBoard } from "./board.js";
+import { drawBoard } from "./render.js";
 
-// export function runGame(board, canvas) {
-//   let currentTimeou = 800;
-//   tetromineFallDown(board, currentTimeou);
-// }
+export function runGame(board, canvas) {
+  let currentTimeou = 800;
+  tetromineFallDown(board, currentTimeou);
+}
 
-// export function tetromineFallDown(board, timeout) {
-//   setTimeout(() => {
-//     if (isBoardAdvanceable(board)) {
-//       advanceBoard(board);
-//       drawBoard(board, canvas);
-//       tetromineFallDown(board, timeout);
-//     } else {
-//       return false;
-//     }
-//   }, timeout);
-// }
+export function tetromineFallDown(board, timeout) {
+  setTimeout(() => {
+    if (isBoardAdvanceable(board)) {
+      advanceBoard(board);
+      drawBoard(board, canvas);
+      tetromineFallDown(board, timeout);
+    } else {
+      return false;
+    }
+  }, timeout);
+}
--- js/main.js
+++ js/main.js
@@ -2,21 +2,25 @@ import { createBoard, registerControlFunctions } from "./board.js";
 import { drawBoard } from "./render.js";
 import { createTetromino } from "./tetrominos.js";
 import { advanceBoard } from "./board.js";
+import { runGame } from "./game.js";
 
 const canvas = document.getElementById("canvas");
 
 const board = createBoard(20, 10);
 registerControlFunctions(board);
 
-console.log(board);
-drawBoard(board, canvas);
 createTetromino(board, "I");
-drawBoard(board, canvas);
-advanceBoard(board);
-drawBoard(board, canvas);
-advanceBoard(board);
-drawBoard(board, canvas);
-advanceBoard(board);
-drawBoard(board, canvas);
-advanceBoard(board);
-drawBoard(board, canvas);
+runGame(board, canvas);
+
+// console.log(board);
+// drawBoard(board, canvas);
+
+// drawBoard(board, canvas);
+// advanceBoard(board);
+// drawBoard(board, canvas);
+// advanceBoard(board);
+// drawBoard(board, canvas);
+// advanceBoard(board);
+// drawBoard(board, canvas);
+// advanceBoard(board);
+// drawBoard(board, canvas);
